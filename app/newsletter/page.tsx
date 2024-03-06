5688a8cafe4d106dbf9732984e06b8e95e922383
Larry Kann <kanndide@gmail.com>
2024-03-06 17:26:09 -0500

    trying to fix key prop issue for deployment

--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -10,28 +10,30 @@ export default async function Newsletter() {
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="max-w-screen-md">
                 {articles.map((article, index: number) => (
-                    <Link 
-                        href={article.link}
-                        className="border border-transparent rounded-lg px-5 hover:border-gray-300 flex grid lg:grid-cols-2 place-items-center p-4"
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                    >
-                        {article.thumbnail && (
-                            <Image 
-                                src={article.thumbnail} 
-                                width={150}
-                                height={150}
-                                alt="Thumbnail" 
-                                style={{ marginRight: '10px' }} 
-                            />
-                        )}
-                        <div className="max-w-prose" key={index}>
-                            <a href={article.link} target="_blank" rel="noopener noreferrer">
-                                <h2 className="mb-3 text-xl font-semibold p-2">{article.title}</h2>
-                            </a>
-                            <p className="text-sm">{article.description}</p>
-                        </div>
-                    </Link>
+                    <div key={index}>
+                        <Link 
+                            href={article.link}
+                            className="border border-transparent rounded-lg px-5 hover:border-gray-300 flex grid lg:grid-cols-2 place-items-center p-4"
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                        >
+                            {article.thumbnail && (
+                                <Image 
+                                    src={article.thumbnail} 
+                                    width={150}
+                                    height={150}
+                                    alt="Thumbnail" 
+                                    style={{ marginRight: '10px' }} 
+                                />
+                            )}
+                            <div className="max-w-prose">
+                                <a href={article.link} target="_blank" rel="noopener noreferrer">
+                                    <h2 className="mb-3 text-xl font-semibold p-2">{article.title}</h2>
+                                </a>
+                                <p className="text-sm">{article.description}</p>
+                            </div>
+                        </Link>
+                    </div>
                 ))}
             </div>
         </main>
