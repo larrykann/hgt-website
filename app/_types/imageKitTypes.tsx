2255d434cff9215dd43878a31deb0dfd9a244662
Larry Kann <kanndide@gmail.com>
2024-03-07 09:39:02 -0500

    still working types

--- app/_types/imageKitTypes.tsx
+++ app/_types/imageKitTypes.tsx
@@ -1,33 +1,6 @@
 interface ImageKitFile {
   fileId: string;
-  type: string;
   name: string;
-  filePath: string;
-  tags?: string[];
-  AITags?: Array<{
-    name: string;
-    confidence: number;
-    source: string;
-  }>;
-  versionInfo?: {
-    id: string;
-    name: string;
-  };
-  isPrivateFile: boolean;
-  customCoordinates: null | string;
-  url: string;
-  thumbnail: string;
-  fileType: string;
-  mime: string;
-  width: number;
-  height: number;
-  size: number;
-  hasAlpha: boolean;
-  customMetadata?: {
-    [key: string]: any;
-  };
-  createdAt: string;
-  updatedAt: string;
 }
 
 export type ImageKitListFilesResponse = ImageKitFile[];
